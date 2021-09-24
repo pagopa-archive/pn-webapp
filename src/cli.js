5b9a0b2482f013c9183d4147c54597ebfd35e08c
alfonsogiustini <a.giustini@sirti.it>
2021-09-24 15:48:26 +0200

    implementazione pagina receiver.html per download documenti

--- src/cli.js
+++ src/cli.js
@@ -11,18 +11,26 @@ const argv = yargs(hideBin(process.argv)).argv
 sendNotification();
 
 function sendNotification() {
-    let paId = "pa_id";
-    let apiKey = "apiKey";
+    let hostname = "localhost";
+    let port = "8080";
 
-    let buf = fs.readFileSync("src/TSY-Scheda RiepilogoDUB-01-1.0.pdf");
+    let paId = argv.paId == undefined ? "pa_id" : argv.paId;
+    let apiKey = argv.apiKey == undefined ? "apiKey" : argv.apiKey;
+
+    let buf = fs.readFileSync("src/tmp/temp.pdf");
     let sha256 = crypto.createHash('sha256').update(buf).digest('hex');
     let base64 = buf.toString('base64');
 
-    let recipientsCount = argv.recipient.taxId.length;
-    let documentsCount = argv.documents.digests.sha256.length;
+    let recipientIsArray = Array.isArray(argv.recipient.taxId);
+    let documentIsArray = Array.isArray(argv.documents.digests.sha256);
 
-    if (recipientsCount > 5 || documentsCount > 5) {
-        console.log("Sono ammessi non piu di 5 recipients o allegati");
+    if (recipientIsArray && recipientIsArray.length > 5) {
+        console.log("Sono ammessi non piu' di 5 Destinatari");
+        process.exit(1);
+    }
+
+    if (documentIsArray && documentIsArray.length > 5) {
+        console.log("Sono ammessi non piu' di 5 Allegati");
         process.exit(1);
     }
 
@@ -37,39 +45,72 @@ function sendNotification() {
         documents: []
     };
 
-    for (let i = 0; i < recipientsCount; i++) {
+    if (recipientIsArray) {
+        let recipientsCount = argv.recipient.taxId.length;
+        for (let i = 0; i < recipientsCount; i++) {
+            requestBody.recipients.push({
+                taxId: recipient.taxId[i],
+                denomination: recipient.denomination[i],
+                digitalDomicile: {
+                    type: recipient.digitalDomicile.type[i] ,
+                    address: recipient.digitalDomicile.address[i] 
+                },
+                physicalAddress: {
+                    at: recipient.physicalAddress.at[i],
+                    address: recipient.physicalAddress.address[i],
+                    addressDetails: recipient.physicalAddress.addressDetails[i],
+                    zip: recipient.physicalAddress.zip[i],
+                    municipality: recipient.physicalAddress.municipality[i],
+                    province: recipient.physicalAddress.province[i]
+                }
+            })
+        }
+    } else {
         requestBody.recipients.push({
-            taxId: recipient.taxId[i],
-            denomination: recipient.denomination[i],
+            taxId: recipient.taxId,
+            denomination: recipient.denomination,
             digitalDomicile: {
-                type: recipient.digitalDomicile.type[i] ,
-                address: recipient.digitalDomicile.address[i] 
+                type: recipient.digitalDomicile.type,
+                address: recipient.digitalDomicile.address 
             },
             physicalAddress: {
-                at: recipient.physicalAddress.at[i],
-                address: recipient.physicalAddress.address[i],
-                addressDetails: recipient.physicalAddress.addressDetails[i],
-                zip: recipient.physicalAddress.zip[i],
-                municipality: recipient.physicalAddress.municipality[i],
-                province: recipient.physicalAddress.province[i]
+                at: recipient.physicalAddress.at,
+                address: recipient.physicalAddress.address,
+                addressDetails: recipient.physicalAddress.addressDetails,
+                zip: recipient.physicalAddress.zip,
+                municipality: recipient.physicalAddress.municipality,
+                province: recipient.physicalAddress.province
             }
-        })
-    }
+        });
+    }    
 
-    for (let j = 0; j < documentsCount; j++) {
+    if (documentIsArray) {
+        let documentsCount = argv.documents.digests.sha256.length;
+        for (let j = 0; j < documentsCount; j++) {
+            requestBody.documents.push({
+                body: base64,
+                contentType: documents.contentType[j],
+                digests : {
+                    sha256: sha256
+                }
+            });
+        }
+    } else {
         requestBody.documents.push({
             body: base64,
-            contentType: documents.contentType[j],
-            digests : {
+            contentType: documents.contentType,
+            digests: {
                 sha256: sha256
             }
         });
     }
 
     const data = JSON.stringify( requestBody );
+    //console.log(data);
+    
     const options = {
-        hostname: 'localhost',
-        port: 8080,
+        hostname: hostname,
+        port: port,
         path: '/delivery/notifications/sent',
         method: 'POST',
         headers: {
